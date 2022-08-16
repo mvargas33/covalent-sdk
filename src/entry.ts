2c91a315249f356e65deae06478f1ad2ef9aeddc
mvargas33 <mvargas33@protonmail.com>
2022-08-16 18:53:10 -0400

    Add balances calc

--- src/entry.ts
+++ src/entry.ts
@@ -1,5 +1,44 @@
 import { TransactionsResponse, BlockTransactionWithLogEvents } from './models';
 import CovalentTransactions from './covalent/CovalentTransactions';
+import * as fs from 'fs';
+const {
+  BigNumber,
+} = require("@ethersproject/bignumber");
+
+interface UserBalance {
+  timestamp: string;
+  tokens: Record<string, string>;
+}
+
+const calculateUserBalance = (userAddress: string, transactions: Transaction[]): UserBalance[] => {
+  const address = userAddress.toLowerCase();
+  let balances: UserBalance[] = [];
+  transactions.map((tx) => {
+    let tokens: Record<string, string> = {}
+    tx.transfers?.map((transfer) => {
+      if (transfer.to_address.toLowerCase() === address){
+        if (tokens.hasOwnProperty(transfer.token.contract)) {
+          const newAmount = BigNumber.from(tokens[transfer.token.contract]).add(BigNumber.from(transfer.amount)).toString(); // Use big number
+          tokens = {...tokens, [transfer.token.contract]: newAmount}
+        } else {
+          tokens = {...tokens, [transfer.token.contract]: transfer.amount}
+        }
+      } else if (transfer.from_address.toLowerCase() === address) {
+        if (tokens.hasOwnProperty(transfer.token.contract)) {
+          const newAmount = BigNumber.from(tokens[transfer.token.contract]).sub(BigNumber.from(transfer.amount)).toString(); // Use big number
+          tokens = {...tokens, [transfer.token.contract]: newAmount}
+        } else {
+          tokens = {...tokens, [transfer.token.contract]: BigNumber.from(transfer.amount).mul(BigNumber.from(-1)).toString()}
+        }
+      }
+    })
+    balances.push({
+      timestamp: tx.timestamp,
+      tokens
+    })
+  })
+  return balances;
+}
 
 interface TokenInBasket {
   name: string | null;
@@ -14,6 +53,7 @@ interface Transfer {
   token: TokenInBasket;
   amount: string;
   amount_usd: number;
+  log_offset?: number;
 }
 
 interface Transaction {
@@ -33,8 +73,9 @@ const simplyfyResponse = (transactions: BlockTransactionWithLogEvents[]): Transa
     }
     let transfers: Transfer[] = [];
     tx.log_events.forEach((event) => {
+      let newTransfer: Transfer;
       if (event.decoded === null && tx.value !== "0" && tx.value_quote > 0 && event.sender_name !== null) { // Native token tx
-        const newTransfer = {
+        newTransfer = {
           from_address: tx.from_address,
           to_address: tx.to_address,
           token: {
@@ -45,11 +86,27 @@ const simplyfyResponse = (transactions: BlockTransactionWithLogEvents[]): Transa
           },
           amount: tx.value,
           amount_usd: tx.value_quote,
+          log_offset: event.log_offset,
         }
-        const transfers_str = transfers.map(transfer => JSON.stringify(transfer))
-        if (transfers_str.indexOf(JSON.stringify(newTransfer)) == -1) {
+        const transfers_str = transfers.map(transfer => JSON.stringify({...transfer, log_offset: 0}))
+        if (transfers_str.indexOf(JSON.stringify({...newTransfer, log_offset: 0})) == -1) {
           transfers.push(newTransfer)
         }
+      } else if (event.decoded !== null && event.decoded.name === "Transfer") {
+        newTransfer = {
+          from_address: event.decoded.params[0].value,
+          to_address: event.decoded.params[1].value,
+          token: {
+            name: event.sender_name,
+            symbol: event.sender_contract_ticker_symbol,
+            logo_url: event.sender_logo_url,
+            contract: event.sender_address,
+          },
+          amount: event.decoded.params[2].value,
+          amount_usd: 0,
+          log_offset: event.log_offset,
+        }
+        transfers.push(newTransfer)
       }
     })
     txs.push({...cleanTx, transfers});
@@ -64,8 +121,11 @@ const getTxs = async (address: string) => {
   console.log(2)
   const transaction = new CovalentTransactions({ key: API_KEY }, 137);
   const result: TransactionsResponse = await transaction.get(ADDRESS);
+  fs.writeFile(`history.json`, JSON.stringify(result, null, 2), (err) => {
+    if(err) console.log('error', err);
+  });
   // console.log(JSON.stringify(result, null, 2))
-  console.log(JSON.stringify(simplyfyResponse(result.items), null, 2));
+  console.log(JSON.stringify(calculateUserBalance('0xcD2F28b4363c183f8798d5d39d0bcE6a0ffC71B5',simplyfyResponse(result.items)), null, 2));
   return result;
 }
 
